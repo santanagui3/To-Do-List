abadaea1cb17b9339794ecdb0988b9794b16e3ae
Guilherme Santana Dias <75275223+santanagui3@users.noreply.github.com>
2021-03-11 01:48:59 -0300

    To Do List with fake api
    using fake-api

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import { createApp } from 'vue'
+import Todos from './api/todos'
+import './assets/css/style.css'
+
+const apiTodos = new Todos()
+
+const app = createApp({
+    data()  { 
+            return{
+                todos: [],
+                form: {
+                    text: '',
+                    done: false
+                }
+            }
+        },
+        async created() {
+            this.todos =  await apiTodos.index()
+        },
+        methods: {
+            async fetchTodos() {
+                this.todos = await apiTodos.index()
+                console.log(this.todos)
+            },
+            async createTodo() {
+                const data = await apiTodos.store(this.form)
+                this.todos.push(data)
+
+                this.form.text = ''
+                this.form.done = false
+            },
+            async toggleTodoStatus(todo) {
+                const  data = await apiTodos.update({
+                    ... todo,
+                    done: !todo.done 
+                })
+
+                const index = this.todos.findIndex(({id}) => id == data.id)
+                this.todos[index] = data
+
+            },
+            async destroy(id) {
+               await apiTodos.destroy({ id })
+
+                const index = this.todos.findIndex((todo) => todo.id == id)
+                this.todos.splice(index, 1)
+            
+            },
+        },
+    })
+
+app.mount('#app')
